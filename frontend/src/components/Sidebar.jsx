8db487fd08f34ec4518970e58c880374f21719d5
Angelo Benedict S. Galvan <angelobenedictg@gmail.com>
2026-09-17 21:57:06 -0700

    Added logo, fixed formatting a little with sidebar component

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,20 +1,30 @@
+import { useState } from 'react'
+import treeLogo from "../assets/treeLogo.png";
+import wordLogo from "../assets/wordLogo.png";
+
 function Sidebar(){
+
+    const [isCollapsed, setIsCollapsed] = useState(false)
+
     return (
         //flex and flex-col required for vertical gaps
         <div className="
-        flex
+        hidden 
+        md:flex
         flex-col
         h-full
-        w-80
-        bg-slate-800 
+        w-90
+        bg-[#262626] 
         text-white 
         p-6
         gap-4
-        hidden md:block
         ">
-            <h1 className="font-bold text-4xl">
-                Sustainability Bin Tracker
-            </h1>
+            <img
+                src={wordLogo}
+                alt='Sacramento State Sustainability'
+                className="h-32 w-auto object-contain object-[-20%_center]">
+                      
+            </img>
 
             <p className="text-emerald-400 text-xl">
                 Coming soon.
