5ba4dd6d534076a7aeb0f3e925cd3d2b3fde9795
Angelo Benedict S. Galvan <angelobenedictg@gmail.com>
2026-09-19 00:34:14 -0700

    Added mobile/small screen sidebar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Sidebar from './components/Sidebar';
+import MobileNav from './components/MobileNav.jsx';
 import DashboardMap from './pages/DashboardMap.jsx';
 import { useBins } from './hooks/useBins.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -13,7 +14,8 @@ function App() {
     <BrowserRouter>
       <div className="h-screen w-screen flex flex-row font-sans">
         <Sidebar/>
-
+        <MobileNav/>
+        
         <Routes>
           <Route path="/" element={<DashboardMap bins={bins} />}/>
           <Route path="/analytics" element={<Analytics/>} />
---
+++ frontend/src/components/MobileNav.jsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { Link } from "react-router-dom";
+
+function MobileNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="md:hidden">
+      <button
+        onClick={() => setIsOpen(true)}
+        className={`
+            md:hidden 
+            fixed 
+            top-2
+            left-2 
+            z-[2000] 
+            p-1 
+            bg-[#1E1E1E]/40 
+            text-white 
+            rounded-lg 
+            cursor-pointer
+            ${isOpen ? "hidden" : "fixed"}`}
+      >
+        <Menu />
+      </button>
+
+      {isOpen && (
+        <div className="fixed inset-0 z-[3000] flex items-start justify-left bg-black/40 backdrop-blur-xs p-2">
+          <div className="relative bg-[#1E1E1E]/80 backdrop-blur-md border border-white/10 p-6 rounded-2xl flex flex-col gap-6 w-3/4 max-w-xs shadow-2xl">
+            <button
+              onClick={() => setIsOpen(false)}
+              className="absolute top-4 right-4 text-gray-400 cursor-pointer"
+            >
+              <X />
+            </button>
+
+            <nav className="flex-1 flex-col gap-2 px-1">
+              <Link
+                onClick={() => setIsOpen(false)}
+                to="/"
+                className={`  flex 
+                        items-center 
+                        py-2 
+                        rounded-lg 
+                        hover:bg-gradient-to-r
+                        hover:from-emerald-900/80
+                        hover:to-emerald-900/20
+                        transition-colors 
+                        text-white 
+`}
+              >
+                <span className={`font-medium whitespace-nowrap text-gray-300 pl-2`}>
+                Dashboard
+                </span>
+              </Link>
+
+              <Link
+                onClick={() => setIsOpen(false)}
+                to="/analytics"
+                className={`  flex 
+                        items-center 
+                        py-2 
+                        rounded-lg 
+                        hover:bg-gradient-to-r
+                        hover:from-emerald-900/80
+                        hover:to-emerald-900/20
+                        transition-colors 
+                        text-white 
+`}
+              >
+                <span className={`font-medium whitespace-nowrap text-gray-300 pl-2`}>
+                  Analytics
+                </span>
+              </Link>
+
+              <Link
+                onClick={() => setIsOpen(false)}
+                to="/maintenance"
+                className={`  flex 
+                        items-center 
+                        py-2 
+                        rounded-lg 
+                        hover:bg-gradient-to-r
+                        hover:from-emerald-900/80
+                        hover:to-emerald-900/20
+                        transition-colors 
+                        text-white 
+`}
+              >
+                <span className={`font-medium whitespace-nowrap text-gray-300 pl-2`}>
+                  Maintenance
+                </span>
+              </Link>
+
+              <Link
+                onClick={() => setIsOpen(false)}
+                to="/settings"
+                className={`  flex 
+                        items-center 
+                        py-2 
+                        rounded-lg 
+                        hover:bg-gradient-to-r
+                        hover:from-emerald-900/80
+                        hover:to-emerald-900/20
+                        transition-colors 
+                        text-white 
+`}
+              >
+                <span className={`font-medium whitespace-nowrap text-gray-300 pl-2`}>
+                  Settings
+                </span>
+              </Link>
+            </nav>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MobileNav;
