a4c69d160c6e1b1f7a19bb297cf50ea6d9b4b24c
Angelo Benedict S. Galvan <angelobenedictg@gmail.com>
2026-09-21 23:56:05 -0700

    Started analytics page

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -5,15 +5,28 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 function Analytics(){
     return (
         //flex 1 since its going to be under a flex parent
-        <div className="flex-1 bg-slate-800 text-white p-10 h-full">
-            <h1 className="text-4xl font-bold text-emerald-400 mb-6">
+        <div className="flex-1 bg-[#262626] text-white p-10 h-full">
+            <h1 className="text-4xl font-bold text-[#F2F2F3] mb-6">
                 Analytics Dashboard
             </h1>
-            <p className="text-xl">
-                Assigned to: <br/>
-                Build possible useful data based on bins.
-                Could be: Battery health, average full percentage, etc.
-            </p>
+
+            <div className="
+                grid
+                grid-cols-3
+                gap-4">
+                <div className="
+                    bg-[#1E1E1E]
+                    rounded-xl
+                    ">
+                    <h2 className="
+                        text-[#F2F2F3]
+                        text-2xl
+                        font-bold
+                        p-4">
+                        Empty Bins
+                    </h2>
+                </div>
+            </div>
         </div>
     )    
 }
